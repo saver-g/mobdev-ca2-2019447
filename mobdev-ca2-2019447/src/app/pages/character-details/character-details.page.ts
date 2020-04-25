55c84638b4cc143d94dd4a81d84c3b6f1f3ef91f
botond-r <botond.reisenleitner@gmail.com>
2020-04-25 20:14:28 +0000

    set up favourite in character-details

--- mobdev-ca2-2019447/src/app/pages/character-details/character-details.page.ts
+++ mobdev-ca2-2019447/src/app/pages/character-details/character-details.page.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from './../../services/api.service';
+import { FavouriteService } from './../../services/favourite.service';
 
 @Component({
-  selector: 'app-character-details',
-  templateUrl: './character-details.page.html',
-  styleUrls: ['./character-details.page.scss'],
+    selector: 'app-character-details',
+    templateUrl: './character-details.page.html',
+    styleUrls: ['./character-details.page.scss'],
 })
+
 export class CharacterDetailsPage implements OnInit {
+    character: any;
+    characterId = null;
+    isLiked = false;
 
-  constructor() { }
+    constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private favouriteService: FavouriteService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.characterId = this.activatedRoute.snapshot.paramMap.get('id');
+        this.api.getCharacter(this.characterId).subscribe(res => {
+            this.character = res[0];
+            console.log(JSON.stringify(this.character.characterId));
+        })
 
+        this.favouriteService.isLiked(this.characterId).then(isFav => {
+            this.isLiked = isFav;
+        })
+    }
+
+    favouriteCharacter() {
+        this.favouriteService.favouriteCharacter(this.characterId).then(() => {
+            this.isLiked = true;
+        });
+    }
+
+    unfavouriteCharacter() {
+        this.favouriteService.unfavouriteCharacter(this.characterId).then(() => {
+            this.isLiked = false;
+        });
+    }
 }
