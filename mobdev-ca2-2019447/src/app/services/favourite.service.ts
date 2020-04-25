7a3d6c874b96e574e60ab90bdd203d9ca1776944
botond-r <botond.reisenleitner@gmail.com>
2020-04-25 19:28:24 +0000

    set up favourite service

--- mobdev-ca2-2019447/src/app/services/favourite.service.ts
+++ mobdev-ca2-2019447/src/app/services/favourite.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { Storage } from '@ionic/storage';
 
-const STORAGE_KEY = 'favouriteEpisodes';
+const STORAGE_KEY_1 = 'favouriteEpisodes';
+const STORAGE_KEY_2 = 'favouriteCharacters';
+const STORAGE_KEY_3 = 'favouriteQuotes';
 
 @Injectable({
     providedIn: 'root'
@@ -10,8 +12,11 @@ export class FavouriteService {
 
     constructor(private storage: Storage) { }
 
+
+    // A. Favourite Episodes
+    
     getAllFavouriteEpisodes() {
-        return this.storage.get(STORAGE_KEY);
+        return this.storage.get(STORAGE_KEY_1);
     }
 
     isFavourite(episodeId) {
@@ -24,9 +29,9 @@ export class FavouriteService {
         return this.getAllFavouriteEpisodes().then(result => {
             if (result) {
                 result.push(episodeId);
-                return this.storage.set(STORAGE_KEY, result);
+                return this.storage.set(STORAGE_KEY_1, result);
             } else {
-                return this.storage.set(STORAGE_KEY, [episodeId]);
+                return this.storage.set(STORAGE_KEY_1, [episodeId]);
             }
         });
     }
@@ -36,7 +41,75 @@ export class FavouriteService {
             if (result) {
                 var index = result.indexOf(episodeId);
                 result.splice(index, 1);
-                return this.storage.set(STORAGE_KEY, result);
+                return this.storage.set(STORAGE_KEY_1, result);
+            }
+        });
+    }
+
+
+    // B. Favourite Characters
+
+    getAllFavouriteCharacters() {
+        return this.storage.get(STORAGE_KEY_2);
+    }
+
+    isLiked(characterId) {
+        return this.getAllFavouriteCharacters().then(result => {
+            return result && result.indexOf(characterId) !== -1;
+        });
+    }
+
+    favouriteCharacter(characterId) {
+        return this.getAllFavouriteCharacters().then(result => {
+            if (result) {
+                result.push(characterId);
+                return this.storage.set(STORAGE_KEY_2, result);
+            } else {
+                return this.storage.set(STORAGE_KEY_2, [characterId]);
+            }
+        });
+    }
+
+    unfavouriteCharacter(characterId) {
+        return this.getAllFavouriteCharacters().then(result => {
+            if (result) {
+                var index = result.indexOf(characterId);
+                result.splice(index, 1);
+                return this.storage.set(STORAGE_KEY_2, result);
+            }
+        });
+    }
+
+
+    // C. Favourite Quotes
+
+    getAllFavouriteQuotes() {
+        return this.storage.get(STORAGE_KEY_3);
+    }
+
+    isFavourited(quoteId) {
+        return this.getAllFavouriteQuotes().then(result => {
+            return result && result.indexOf(quoteId) !== -1;
+        });
+    }
+
+    favouriteQuote(quoteId) {
+        return this.getAllFavouriteQuotes().then(result => {
+            if (result) {
+                result.push(quoteId);
+                return this.storage.set(STORAGE_KEY_3, result);
+            } else {
+                return this.storage.set(STORAGE_KEY_3, [quoteId]);
+            }
+        });
+    }
+
+    unfavouriteQuote(quoteId) {
+        return this.getAllFavouriteQuotes().then(result => {
+            if (result) {
+                var index = result.indexOf(quoteId);
+                result.splice(index, 1);
+                return this.storage.set(STORAGE_KEY_3, result);
             }
         });
     }
