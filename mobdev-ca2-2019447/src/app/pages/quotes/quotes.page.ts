92b0faf7bda3d1500ff77ae9b9cd2eababb3c3c4
saver-g <saverentec@gmail.com>
2020-04-30 17:57:46 +0000

    tried incorporating searchbar for quotes

--- mobdev-ca2-2019447/src/app/pages/quotes/quotes.page.ts
+++ mobdev-ca2-2019447/src/app/pages/quotes/quotes.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../../services/api.service';
 
 @Component({
     selector: 'app-quotes',
@@ -11,17 +11,30 @@ import { HttpClient } from '@angular/common/http';
 
 export class QuotesPage implements OnInit {
 
+    // Variables declared
     quotes: Observable<any>;
+    quotes_array: any[] = [];
+    findKey = '';
 
-    constructor(private router: Router, private http: HttpClient) { }
+    constructor(private router: Router, private api: ApiService) { }
 
+    // Initialisation
     ngOnInit() {
-        this.quotes = this.http.get('https://breakingbadapi.com/api/quotes');
+        this.quotes = this.api.getQuotes();
         this.quotes.subscribe(data => {
-            console.log('my data: ', data);
+            console.log(this.quotes_array);
         });
     }
 
+    getQuoteKey(event) {
+        const keyword = event.target.value;
+        this.findKey = keyword;
+        console.log(keyword);
+    }
+
+
+
+    // Retrieves quotes' id by 'routing' to a new page (details)
     openDetails(quote) {
         let quoteId = quote.quote_id;
         this.router.navigateByUrl(`/tabs/quotes/${quoteId}`);
