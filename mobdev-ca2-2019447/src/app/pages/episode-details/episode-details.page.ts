52f23eb242f630fa15e3d8023361f0e49871a6d3
saver-g <saverentec@gmail.com>
2020-04-30 23:25:08 +0000

    improved design of episode-details

--- mobdev-ca2-2019447/src/app/pages/episode-details/episode-details.page.ts
+++ mobdev-ca2-2019447/src/app/pages/episode-details/episode-details.page.ts
@@ -10,30 +10,41 @@ import { FavouriteService } from './../../services/favourite.service';
 })
 
 export class EpisodeDetailsPage implements OnInit {
+
+    // Variables declared
     episode: any;
     episodeId = null;
     isFavourite = false;
+    characters = [];
 
     constructor(private activatedRoute: ActivatedRoute, private api: ApiService, private favouriteService: FavouriteService) { }
 
+    // Initialisation
     ngOnInit() {
         this.episodeId = this.activatedRoute.snapshot.paramMap.get('id');
         this.api.getEpisode(this.episodeId).subscribe(res => {
             this.episode = res[0];
             console.log(JSON.stringify(this.episode.episode_id));
+
+            // Lists all characters, each in a separate line
+            this.characters = res[0].characters;
+            console.log("character:", this.characters);
         })
 
+        // Function to check whether episode is marked as favourite
         this.favouriteService.isFavourite(this.episodeId).then(isFav => {
             this.isFavourite = isFav;
         })
     }
 
+    // Function to favourite episodes
     favouriteEpisode() {
         this.favouriteService.favouriteEpisode(this.episodeId).then(() => {
             this.isFavourite = true;
         });
     }
 
+    // Function to unfavourite episodes
     unfavouriteEpisode() {
         this.favouriteService.unfavouriteEpisode(this.episodeId).then(() => {
             this.isFavourite = false;
